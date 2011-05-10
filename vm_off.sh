0cce4861c6d54d09c19c722ad341afba50adc438
netinfox <netinfox@gmail.com>
2011-05-11 05:51:42 +0800

    add Mac OS X's VM on/off controls

---
+++ vm_off.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
+
---
+++ vm_on.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
+
